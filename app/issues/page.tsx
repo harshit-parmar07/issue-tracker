ec4c423c019263a0ad912278469d97a13be62e9e
Harshit Singh Parmar <hsp9696@gmail.com>
2025-10-23 20:48:15 +0530

    show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -24,7 +24,10 @@ const IssuesPage = async () => {
           {
             issues.map((issue) => (
               <Table.Row key={issue.id}>
-                <Table.Cell>{issue.title}
+                <Table.Cell>
+                  <Link href={`issues/${issue.id}`}>
+                    {issue.title}
+                  </Link>
                   <div className='block md:hidden'>
                     <IssueStatusBadge status={issue.status} />
                   </div>
