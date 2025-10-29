29ba1e853904582ffc9a56bb571c8ba762607d9f
Harshit Singh Parmar <hsp9696@gmail.com>
2025-10-29 21:04:44 +0530

    adding login and logout links

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
-// import AuthProvider from "./auth/Provider";
+import AuthProvider from "./auth/Provider";
 // import QueryClientProvider from "./QueryClientProvider";
 
 const inter = Inter({
@@ -26,16 +26,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        {/* <QueryClientProvider>
-          <AuthProvider> */}
-        <Theme accentColor="violet">
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
-        {/* </AuthProvider>
-        </QueryClientProvider> */}
+        {/* <QueryClientProvider> */}
+        <AuthProvider>
+          <Theme accentColor="violet">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
+        {/* </QueryClientProvider> */}
       </body>
     </html>
   );
---
+++ prisma/migrations/20251028192446_fix/migration.sql
@@ -0,0 +1,89 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `VerificationRequest` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `Account` DROP FOREIGN KEY `Account_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `Session` DROP FOREIGN KEY `Session_userId_fkey`;
+
+-- DropTable
+DROP TABLE `Account`;
+
+-- DropTable
+DROP TABLE `Session`;
+
+-- DropTable
+DROP TABLE `User`;
+
+-- DropTable
+DROP TABLE `VerificationRequest`;
+
+-- CreateTable
+CREATE TABLE `accounts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `compound_id` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `provider_type` VARCHAR(191) NOT NULL,
+    `provider_id` VARCHAR(191) NOT NULL,
+    `provider_account_id` VARCHAR(191) NOT NULL,
+    `refresh_token` VARCHAR(191) NULL,
+    `access_token` VARCHAR(191) NULL,
+    `access_token_expires` DATETIME(3) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `accounts_compound_id_key`(`compound_id`),
+    INDEX `providerAccountId`(`provider_account_id`),
+    INDEX `providerId`(`provider_id`),
+    INDEX `userId`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sessions` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `session_token` VARCHAR(191) NOT NULL,
+    `access_token` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `sessions_session_token_key`(`session_token`),
+    UNIQUE INDEX `sessions_access_token_key`(`access_token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `email_verified` DATETIME(3) NULL,
+    `image` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `verification_requests` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `identifier` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `verification_requests_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
