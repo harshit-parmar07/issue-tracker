9d3c46ff62a91f0ed22a9c8bd11cde289c65bea6
Harshit Singh Parmar <hsp9696@gmail.com>
2025-11-02 16:45:52 +0530

    Fetch data with react query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,21 +2,34 @@
 
 import { User } from '@prisma/client';
 import { Select } from "@radix-ui/themes";
+import { useQuery } from '@tanstack/react-query';
 // import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, } from '@radix-ui/themes'
 // import * as Select from "@radix-ui/react-select";
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import { useEffect } from 'react';
+import { Skeleton } from '@/app/components';
 
 const AssigneeSelect = () => {
-    const [users, setUsers] = useState<User[]>([]);
+    const { data: users, error, isLoading } = useQuery<User[]>({
+        queryKey: ['users'],
+        queryFn: () => axios.get('/api/users').then(res => res.data),
+        retry: 3,
+        staleTime: 60000
+    });
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const { data } = await axios.get<User[]>('/api/users');
-            setUsers(data);
-        }
-        fetchUsers()
-    }, [])
+    if (isLoading) return <Skeleton />
+
+    if (error) return null
+
+    // const [users, setUsers] = useState<User[]>([]);
+
+    // useEffect(() => {
+    //     const fetchUsers = async () => {
+    //         const { data } = await axios.get<User[]>('/api/users');
+    //         setUsers(data);
+    //     }
+    //     fetchUsers()
+    // }, [])
 
     return (
         <Select.Root>
@@ -26,7 +39,7 @@ const AssigneeSelect = () => {
                     <Select.Label>
                         Suggestions
                     </Select.Label>
-                    {users.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
+                    {users?.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
 
                 </Select.Group>
             </Select.Content>
