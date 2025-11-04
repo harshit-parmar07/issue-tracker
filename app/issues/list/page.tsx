9fae44658ef192094b832c1f98fac2ff97d9a45b
Harshit Singh Parmar <hsp9696@gmail.com>
2025-11-04 20:50:00 +0530

    filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -3,9 +3,16 @@ import { Table } from '@radix-ui/themes';
 import React from 'react';
 import IssueActions from './IssueActions';
 import { Link, IssueStatusBadge } from '@/app/components'
+import { Status } from '@prisma/client';
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+const IssuesPage = async ({ searchParams }: { searchParams: { status: Status } }) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
+  const issues = await prisma.issue.findMany({
+    where: {
+      status
+    }
+  });
   // await delay(2000)
   return (
     <div>
