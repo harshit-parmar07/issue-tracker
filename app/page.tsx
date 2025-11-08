3296c500cee5dd45d01e601a8f79e5d2a36317ad
Harshit Singh Parmar <hsp9696@gmail.com>
2025-11-08 17:31:02 +0530

    Fix: dashboard not updating

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,8 @@ export default async function Home() {
   </Grid>
 }
 
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
   title: 'Issue Tracker - Dashboard',
   description: 'View a summary of project issues'
